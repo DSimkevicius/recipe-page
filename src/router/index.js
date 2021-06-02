eb6a4d54b969a93fe28418623624406392668acd
kobimodel5 <sromasterw@gmail.com>
2021-06-02 16:08:47 +0300

    added login function, some small edit on app.vue, register.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import Home from '../views/Home.vue';
 
 const routes = [
@@ -17,11 +19,26 @@ const routes = [
     name: 'Register',
     component: () => import(/* webpackChunkName: "register" */ '../views/Register.vue'),
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue'),
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (!user && to.meta.requiresAuth) {
+      next('/');
+    } else {
+      next();
+    }
+  });
 });
 
 export default router;
